5d7ee7cc680b073ad57a0ab3c49c17d746efebbd
Rachana5856 <45552880+Rachana5856@users.noreply.github.com>
2026-04-22 18:02:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello from DevOps Demo App \n");
+  res.end("Running with ps \n");
 });
 
 server.listen(PORT, () => {
