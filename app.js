89245a9190770e85aa4d42fe71d2faae35a8a0ce
rachana5856 <rachanakesarkar03@gamil.com>
2026-04-22 18:08:08 +0530

    updates

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from DevOps Demo App \n");
-  res.end("Running with ps \n");
+  res.end("Hello from DevOps Demo App\n Running on pm2 \n");
 });
 
 server.listen(PORT, () => {
